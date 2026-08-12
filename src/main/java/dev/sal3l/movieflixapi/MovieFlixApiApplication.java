f0c17c6e4630166355dd15eaa216c1dd7d9995d2
G. Sales <gustavosales0809@gmail.com>
2026-08-12 19:25:15 -0300

    initial commit

---
+++ src/main/java/dev/sal3l/movieflixapi/MovieFlixApiApplication.java
@@ -0,0 +1,13 @@
+package dev.sal3l.movieflixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFlixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFlixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/sal3l/movieflixapi/MovieFlixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sal3l.movieflixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

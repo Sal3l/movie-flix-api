df15689d9d252893bc443153df622eb8ae04461f
G. Sales <gustavosales0809@gmail.com>
2026-08-14 22:33:08 -0300

    feat: CategoryService

---
+++ src/main/java/dev/sal3l/movieflixapi/service/CategoryService.java
@@ -0,0 +1,35 @@
+package dev.sal3l.movieflixapi.service;
+
+import dev.sal3l.movieflixapi.entity.Category;
+import dev.sal3l.movieflixapi.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category create(Category category) {
+        return repository.save(category);
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> category = repository.findById(id);
+        return category.orElse(null);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}

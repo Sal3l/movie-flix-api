3b4ca5d6c0694f9b57abca6cb07cb88daf8e4d7f
G. Sales <gustavosales0809@gmail.com>
2026-08-19 10:02:49 -0300

    fix: CategoryMapper methods change to static

--- src/main/java/dev/sal3l/movieflixapi/mapper/CategoryMapper.java
+++ src/main/java/dev/sal3l/movieflixapi/mapper/CategoryMapper.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class CategoryMapper {
 
-    public Category map(CategoryRequest request) {
+    public static Category map(CategoryRequest request) {
         return Category
                 .builder()
                 .name(request.name())
                 .build();
     }
 
-    public CategoryResponse map(Category category) {
+    public static CategoryResponse map(Category category) {
         return CategoryResponse
                 .builder()
                 .id(category.getId())

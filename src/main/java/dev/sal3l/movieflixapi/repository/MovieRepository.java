98b1acdf1d82730c4d03c32fef5556548fe7f165
G. Sales <gustavosales0809@gmail.com>
2026-08-20 20:11:48 -0300

    feat: MovieRepository new method findByCategoriesId

--- src/main/java/dev/sal3l/movieflixapi/repository/MovieRepository.java
+++ src/main/java/dev/sal3l/movieflixapi/repository/MovieRepository.java
@@ -4,6 +4,11 @@ import dev.sal3l.movieflixapi.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategoriesId(Long categoryId);
+
 }

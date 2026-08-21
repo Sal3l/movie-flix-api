1fcbc57562df0cef534e48872e2dc0361f4ebc5e
G. Sales <gustavosales0809@gmail.com>
2026-08-21 20:04:36 -0300

    feat: AuthService

---
+++ src/main/java/dev/sal3l/movieflixapi/service/AuthService.java
@@ -0,0 +1,22 @@
+package dev.sal3l.movieflixapi.service;
+
+import dev.sal3l.movieflixapi.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário ou senha invalidos"));
+    }
+
+
+}

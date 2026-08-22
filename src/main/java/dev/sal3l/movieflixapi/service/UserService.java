a452bad4c0e4baa812f67442a998e10181a8f70d
G. Sales <gustavosales0809@gmail.com>
2026-08-21 21:14:07 -0300

    feat: UserService create with password encoding

--- src/main/java/dev/sal3l/movieflixapi/service/UserService.java
+++ src/main/java/dev/sal3l/movieflixapi/service/UserService.java
@@ -2,18 +2,23 @@ package dev.sal3l.movieflixapi.service;
 
 import dev.sal3l.movieflixapi.entity.User;
 import dev.sal3l.movieflixapi.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User create(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return repository.save(user);
     }
 }

c28c78bde1ba3cac6b68fdf8bdc5788bef632ce5
G. Sales <gustavosales0809@gmail.com>
2026-08-21 21:18:23 -0300

    feat: AuthController new route /login

--- src/main/java/dev/sal3l/movieflixapi/controller/AuthController.java
+++ src/main/java/dev/sal3l/movieflixapi/controller/AuthController.java
@@ -1,10 +1,16 @@
 package dev.sal3l.movieflixapi.controller;
 
+import dev.sal3l.movieflixapi.DTO.LoginRequest;
+import dev.sal3l.movieflixapi.DTO.LoginResponse;
 import dev.sal3l.movieflixapi.DTO.UserRequest;
 import dev.sal3l.movieflixapi.DTO.UserResponse;
+import dev.sal3l.movieflixapi.config.TokenService;
 import dev.sal3l.movieflixapi.entity.User;
 import dev.sal3l.movieflixapi.mapper.UserMapper;
 import dev.sal3l.movieflixapi.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/register")
@@ -25,5 +35,16 @@ public class AuthController {
         User entity = UserMapper.map(request);
         User created = userService.create(entity);
         return UserMapper.map(created);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+        String token = tokenService.generateToken(user);
+
+        return new LoginResponse(token);
     }
 }
